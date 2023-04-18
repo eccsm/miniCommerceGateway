af2c67723ef9100f21151f662c1953468a60b2e5
chingeez <cengiz.uzuntas@gmail.com>
2023-04-18 22:57:42 +0300

    initial commit

---
+++ src/main/java/tr/nttdata/poc/minicommerce/minicommercegateway/MiniCommerceGatewayApplication.java
@@ -0,0 +1,13 @@
+package tr.nttdata.poc.minicommerce.minicommercegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCommerceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniCommerceGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tr/nttdata/poc/minicommerce/minicommercegateway/MiniCommerceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.nttdata.poc.minicommerce.minicommercegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCommerceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
